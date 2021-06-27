8a1c3f0440a24ddadc5250285ae5471c9efb485b
csslick <digitone@naver.com>
2021-06-27 22:19:34 +0900

    더보기

--- main.js
+++ main.js
@@ -3,17 +3,16 @@ const API_URL = 'https://pokeapi.co/api/v2/pokemon/'
 function getPokemon(num) {
   fetch(API_URL + num) 
     .then(function(res){
-    return res.json();
-  })
+      return res.json();
+    })
     .then(function(data){
-    console.log(data)
-    const id = data.id;
-    const name = data.name;
-    const imgUrl =  data.sprites.other.dream_world.
-    front_default;
-    const types = data.types;
-    displayPokemon(id, name, imgUrl, types);
-  })
+      // console.log(data)
+      const id = data.id;
+      const name = data.name;
+      const imgUrl =  data.sprites.other.dream_world.front_default;
+      const types = data.types;
+      displayPokemon(id, name, imgUrl, types);
+    })
 }
 
 // default param = 1
@@ -38,7 +37,7 @@ function displayPokemon(id, name, imgUrl, types) {
       </a>  
       <figcaption>
         <span class="id">#${('00'+id).slice(-3)}</span>
-        <h4>name: ${name}</h4>
+        <h4>${name}</h4>
         <span 
           class="type" data-bg='${typeName[0]}'>${typeName[0]}</span>
         ${typeName[1] ? 
@@ -46,8 +45,20 @@ function displayPokemon(id, name, imgUrl, types) {
       </figcaption>
     </figure>
   `
-  const main = document.querySelector('main');
+  const main = document.querySelector('main > section');
   main.innerHTML += html;
 }
 
+
+/*** main start ***/
 getPokemonList();
+
+// 이전/다음 버튼
+let pokemonIdx = 1;
+const nextBtn = document.getElementById('nextBtn');
+
+nextBtn.addEventListener('click', function(){
+  pokemonIdx += 12;
+  getPokemonList(pokemonIdx);
+  console.log(pokemonIdx)
+})
